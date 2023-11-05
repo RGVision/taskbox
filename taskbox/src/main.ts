dc14a017607199864c56e2981f5ab5eca4e990ab
RGVision <junaidjun61@gmail.com>
2023-11-05 11:05:44 +0530

    storybook changes

--- taskbox/src/main.ts
+++ taskbox/src/main.ts
@@ -1,7 +1,18 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+import type { StorybookConfig } from '@storybook/angular';
+const config: StorybookConfig = {
+ stories: ['../src/app/components/**/*.stories.ts'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/angular',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+};
+export default config;
