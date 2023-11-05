9565284c4a31437bb09f4920bc1860fe6704ec5f
RGVision <junaidjun61@gmail.com>
2023-11-05 12:15:07 +0530

    3

--- taskbox/src/main.ts
+++ taskbox/src/main.ts
@@ -1,18 +1,17 @@
-
-import type { StorybookConfig } from '@storybook/angular';
+import type { StorybookConfig } from "@storybook/angular";
 const config: StorybookConfig = {
- stories: ['../src/app/components/**/*.stories.ts'],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
   ],
   framework: {
-    name: '@storybook/angular',
+    name: "@storybook/angular",
     options: {},
   },
   docs: {
-    autodocs: 'tag',
+    autodocs: "tag",
   },
 };
 export default config;
