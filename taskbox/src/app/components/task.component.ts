507cc1d62874f475204879e81b9df2f32d6982f9
RGVision <junaidjun61@gmail.com>
2023-11-05 16:47:43 +0530

    change task background to red

--- taskbox/src/app/components/task.component.ts
+++ taskbox/src/app/components/task.component.ts
@@ -2,6 +2,8 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from '../models/task.model';
 
+
+
 @Component({
   selector: 'app-task',
   template: `
@@ -32,6 +34,7 @@ import { Task } from '../models/task.model';
           id="title-{{ task?.id }}"
           name="title-{{ task?.id }}"
           placeholder="Input title"
+         style="background: red;"
         />
       </label>
       <button
